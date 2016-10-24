b0f805d6d8ab406bc0e9155ef355ee92890e5c92
joey <zhiyiliu1989@yahoo.com>
2016-10-23 17:22:02 -0700

    convert CRUD to use cunrrent user todos only

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/', function(req, res) {
 //GET /todos
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
     var query = req.query;
-    var where = {};
+    var where = {
+        userId: req.user.get('id')
+    };
 
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
         where.completed = true;
@@ -46,7 +48,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
-    db.todo.findById(todoId).then(function(todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userid: req.user.get('id')
+        }
+    }).then(function(todo) {
         if (!!todo) {
             res.json(todo.toJSON());
         } else {
@@ -62,7 +69,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
     db.todo.create(body).then(function(todo) {
-        res.json(todo.toJSON());
+        req.user.addTodo(todo).then(function(){
+            return todo.reload();
+        }).then(function(todo){
+            res.json(todo.toJSON());
+        });
     }, function(e) {
         res.status(400).json(e);
     });
@@ -74,7 +85,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
     db.todo.destroy({
         where: {
-            id: todoId
+            id: todoId,
+            userid: req.user.get('id')
         }
     }).then(function(rowDeleted) {
         if (rowDeleted === 0) {
@@ -103,7 +115,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
         attributes.description = body.description;
     }
 
-    db.todo.findById(todoId).then(function(todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userid: req.user.get('id')
+        }
+    }).then(function(todo) {
         if (todo) {
             todo.update(attributes).then(function(todo) {
                 res.json(todo.toJSON());
@@ -143,7 +160,7 @@ app.post('/users/login', function(req, res){
     });
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
     app.listen(PORT, function() {
         console.log('express listening to port' + PORT);
     });
